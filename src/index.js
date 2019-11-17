07f9f45ada454d9b0feb7b632c03ea82c09d0956
snsaleh1 <snsaleh11@gmail.com>
2019-11-17 17:56:36 -0600

    created empty array for notes in firebase database

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('evernote-container'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
