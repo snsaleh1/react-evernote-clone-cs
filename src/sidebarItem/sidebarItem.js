32bf119ea0fc6538bd18ca205b91ac9ec76174d3
snsaleh1 <snsaleh11@gmail.com>
2019-11-19 15:12:46 -0600

    Are you sure you want to delete

--- src/sidebarItem/sidebarItem.js
+++ src/sidebarItem/sidebarItem.js
@@ -14,10 +14,29 @@ class SidebarItemComponent extends React.Component {
         const { _index, _note, classes, selectedNoteIndex } = this.props;
 
         return(
-            <div>
-
+            <div key={_index}>
+                <ListItem
+                className={classes.listItem}
+                selected={selectedNoteIndex === _index}
+                alignItems='flex-start'>
+                    <div 
+                    className={classes.textSection}
+                    onClick={() => this.selectNote(_note, _index)}>
+                        <ListItemText
+                            primary={_note.title}
+                            secondary={removeHTMLTags(_note.body.substring(0, 30)) + '...'}></ListItemText>
+                    </div>
+                    <DeleteIcon onClick={() => this.deleteNote(_note)}
+                    className={classes.deleteIcon}></DeleteIcon>
+                </ListItem>
             </div>
         );
+    }
+    selectNote = (n, i) => this.props.selectNote(n, i);
+    deleteNote = (note) => {
+        if(window.confirm(`Are you sure you want to delete: ${note.title}`)) {
+            this.props.deleteNote(note);
+        }
     }
 }
 
